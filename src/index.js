f9d350c2f85b7fe9eb2ccd9e733e36fbcf726de5
yelo <zhihuzeye@gmail.com>
2019-06-20 08:03:26 +0800

    feat: update homepage, fix wol calling

--- src/index.js
+++ src/index.js
@@ -10,22 +10,37 @@ const WOLTH_ADDRESS = process.env.WOLTH_ADDRESS || '255.255.255.255'
 const WOLTH_PORT = +process.env.WOLTH_PORT || 9
 
 const help = ({ endpoint }) => `
-# WOLTH
+<html>
+  <head>
+    <title>Wolth</title>
+    <link rel="stylesheet" href="//unpkg.com/hack@0.8.1/dist/hack.css" />
+  </head>
+  <body class="hack">
+    <div class="container">
+      <h1>WOLTH</h1>
+      <p>Wake with curl:</p>
 
-## Usage
-\`\`\`bash
-$ curl -X POST http://${endpoint}/
-\`\`\`
+      <pre>$ curl -X POST http://${endpoint}/</pre>
 
-## More
-[GitHub](https://github.com/imyelo/wolth)
+      <p>Or just a click:</p>
+
+      <form method="POST">
+        <button class="btn btn-primary">Wake</button>
+      </form>
+
+      <hr/>
+
+      <p><a href="https://github.com/imyelo/wolth" target="_blank">README</a></p>
+    </div>
+  </body>
+</html>
 `.trim()
 
 const wolth = async (req) => {
   if (req.method !== 'POST') {
     return help({ endpoint: req.headers.host })
   }
-  await wol(WOLTH_MAC, {
+  await wol.wake(WOLTH_MAC, {
     address: WOLTH_ADDRESS,
     port: WOLTH_PORT,
   })
